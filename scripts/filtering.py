18c4eeb8643cea16101baa2002400a7c395602d1
scai <scai@cmri.org.au>
2019-03-20 16:53:56 +1100

    reorganise. add alternative DEP method

---
+++ scripts/filtering.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+import pandas as pd
+import sys
+
+df = pd.read_csv(sys.argv[1], sep='\t', names=['chr', 'start', 'end', 'value'])
+df_filtered = df[(df['value'] > float(sys.argv[2])) | (df['value'] < -float(sys.argv[2]))]
+df_filtered = df_filtered.reset_index()
+df_filtered.to_csv(sys.argv[3], header=False, sep='\t', index=False, float_format='%.7f',
+                   columns=['chr', 'start', 'end', 'index', 'value'])

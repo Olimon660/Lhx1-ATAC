0bc5dc237d74876537bea59d30b6b9293e0e6a5b
scai <scai@cmri.org.au>
2019-03-23 11:40:33 +1100

    add filtering script for PyDNAse

---
+++ scripts/bed_lengthen.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import sys
+
+# this script is used to filter out peaks that are shorter than 100
+
+bed = pd.read_csv(sys.argv[1], sep='\t', names=['chr', 'start', 'end'])
+res = bed[(bed['end'] - bed['start']) > 100]
+res.to_csv(sys.argv[2], sep='\t', header=False, index=False)
--- scripts/filtering.py
+++ scripts/filtering.py
@@ -3,6 +3,8 @@
 import pandas as pd
 import sys
 
+# this script is used to filter fold change atac bed files for DEP
+
 df = pd.read_csv(sys.argv[1], sep='\t', names=['chr', 'start', 'end', 'value'])
 df_filtered = df[(df['value'] > float(sys.argv[2])) | (df['value'] < -float(sys.argv[2]))]
 df_filtered = df_filtered.reset_index()
